fe286b84bb985ddd61472edaff55f0221e5f3586
hatwell <caroline.hatwell@gmail.com>
2017-02-06 16:36:39 +0000

    make it better

---
+++ tracery_grammar.rb
@@ -0,0 +1,8 @@
+require 'tracery'
+include Tracery
+grammar = createGrammar(
+
+{
+  "spooky_thing" => ["apparition", "ghost", "ghoul", "haint", "haunting", "phantasm", "phantom", "shade", "shadow", "spectre", "spirit", "wraith"] 
+}
+);
