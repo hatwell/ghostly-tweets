3de4d80397decacb1fc8fe57a196055f9daafbda
hatwell <caroline.hatwell@gmail.com>
2017-02-08 22:50:39 +0000

    trying to add to git again

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# haunted-tweets
+
 # ghostly-tweets
+
+This is a very simple twitter bot I made to answer the question "if a ghost could tweet, what would it tweet?"
+
+Sadly, ghosts cannot hold phones and do not have real fingers, so tweeting is out of the question for them.
+
+I learned how to make this twitter bot by following [this excellent tutorial](https://github.com/jenkr55) from [Jennifer Kruse](https://github.com/jenkr55).
+
+I also had some help from [https://github.com/tef](this guy).
+
+Incidentally, this is my first time using markdown, and I used [this great guide](https://daringfireball.net/).
--- test.rb
+++ test.rb
@@ -1,15 +1,3 @@
-require 'rubygems'
-require 'twitter'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = "gyyBWNnKnkxexsWaicceQ3XBF"
-  config.consumer_secret = "4b9i4fk97I9dpQZjJrySjShPUTiG1XiKlJDJ6UZnHouZE1Y1ip"
-  config.access_token = "828582857526370305-CT6485oTc6XiqK1Jb8xhET4sp9RAcP7"
-  config.access_token_secret = "XdyIvTUFYWd2YaySFNiNOwBr3YDAoNwGuYNR9AgjGxRn8"
-end
-
-
-
 tweets = ["life is spooky", "i am in purgatory", "ghosts are everywhere", "ghosts do not fear you, do not fear ghosts", "some ghosts are the ghosts of dogs"]
 
 emoji = ["👻", "💀", "🔮", "⚰️"]
@@ -29,4 +17,4 @@ potential_tweets = ["a #{spooky_things["spooky_adjective"][rand(5)]} #{spooky_th
 
 
 
-client.update(potential_tweets[rand(0..1)])
+ puts potential_tweets[rand(0..1)]
