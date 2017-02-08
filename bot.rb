657f4228a8bafe3103e2debd93bd1c186ea25baa
hatwell <caroline.hatwell@gmail.com>
2017-02-08 16:00:44 +0000

    Added some more options for tweets

--- bot.rb
+++ bot.rb
@@ -24,9 +24,9 @@ spooky_things = {
 
 #puts "a #{spooky_things["spooky_adjective"][rand(4)]} #{spooky_things["spooky_thing"][rand(10)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}  "
 
-potential_tweets = ["a #{spooky_things["spooky_adjective"][rand(4)]} #{spooky_things["spooky_thing"][rand(10)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}", "#{tweets[rand(0..tweets.length-1)]} #{emoji[rand(0..emoji.length-1)]}" ]
+potential_tweets = ["a #{spooky_things["spooky_adjective"][rand(5)]} #{spooky_things["spooky_thing"][rand(12)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}", "#{tweets[rand(0..tweets.length-1)]} #{emoji[rand(0..emoji.length-1)]}" ]
 
 
 
 
-client.update(potential_tweets[rand(0..1)])
+client.update(potential_tweets[rand(0..2)])
