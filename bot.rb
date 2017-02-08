c3d5e8642c45480310255b87cb927b06682c20c0
hatwell <caroline.hatwell@gmail.com>
2017-02-08 15:58:00 +0000

    Added some more options for tweets

--- bot.rb
+++ bot.rb
@@ -14,4 +14,19 @@ tweets = ["life is spooky", "i am in purgatory", "ghosts are everywhere", "ghost
 
 emoji = ["👻", "💀", "🔮", "⚰️"]
 
-client.update("#{tweets[rand(0..tweets.length-1)]} #{emoji[rand(0..emoji.length-1)]}")
+spooky_things = {
+  "spooky_thing" => ["apparition", "ghost", "ghoul", "haint", "phantasm", "phantom", "shade", "shadow", "spectre", "spirit", "wraith"],
+  "spooky_adjective" => ["eerie", "ghastly", "terrifying", "spectral", "supernatural"],
+  "spooky_verb" => ["watching", "haunting", "lurking near"],
+  "spooky_emoji" => ["👻", "💀", "🔮", "⚰️"]
+}
+
+
+#puts "a #{spooky_things["spooky_adjective"][rand(4)]} #{spooky_things["spooky_thing"][rand(10)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}  "
+
+potential_tweets = ["a #{spooky_things["spooky_adjective"][rand(4)]} #{spooky_things["spooky_thing"][rand(10)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}", "#{tweets[rand(0..tweets.length-1)]} #{emoji[rand(0..emoji.length-1)]}" ]
+
+
+
+
+client.update(potential_tweets[rand(0..1)])
--- tracery_grammar.rb
+++ tracery_grammar.rb
@@ -3,6 +3,19 @@ include Tracery
 grammar = createGrammar(
 
 {
-  "spooky_thing" => ["apparition", "ghost", "ghoul", "haint", "haunting", "phantasm", "phantom", "shade", "shadow", "spectre", "spirit", "wraith"] 
+  "spooky_thing" => ["apparition", "ghost", "ghoul", "haint", "phantasm", "phantom", "shade", "shadow", "spectre", "spirit", "wraith"],
+  "spooky_adjective" => ["eerie", "ghastly", "terrifying", "spectral", "supernatural"],
+  "spooky_verb" => ["watching", "haunting", "lurking near"]
 }
 );
+
+
+spooky_things = {
+  "spooky_thing" => ["apparition", "ghost", "ghoul", "haint", "phantasm", "phantom", "shade", "shadow", "spectre", "spirit", "wraith"],
+  "spooky_adjective" => ["eerie", "ghastly", "terrifying", "spectral", "supernatural"],
+  "spooky_verb" => ["watching", "haunting", "lurking near"],
+  "spooky_emoji" => ["👻", "💀", "🔮", "⚰️"]
+}
+
+
+puts "a #{spooky_things["spooky_adjective"][rand(4)]} #{spooky_things["spooky_thing"][rand(10)]} is #{spooky_things["spooky_verb"][rand(3)]} you. #{spooky_things["spooky_emoji"][rand(3)]}  "
